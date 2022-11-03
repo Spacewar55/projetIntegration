5acd014c707716d1baa6ab9e857d5a9a86569784
wilfrid-lv <wilfrid-levalegant@live.fr>
2022-11-03 22:16:30 +0000

    début ajout servWeb, ajout css dans la page html

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
+/* Copyright (C) 2021 Alain Dube
+ * All rights reserved.
+ *
+ * Projet Sac
+ * Ecole du Web
+ * Cours Objets connectés (c)2022
+ *  
+    @file     main.cpp
+    @author   Alain Dubé
+    @version  1.1 21/08/15 
+
+    Historique des versions
+           Version    Date       Auteur       Description
+           1.1        21/08/15  Alain       Première version du logiciel
+
+    platform = espressif32
+    board = esp32doit-devkit-v1
+    framework = arduino
+    lib_deps = 
+                      
+            ESPAsyncWebServer-esphome                   (Pour accéder au Wifi)
+            AsyncTCP-esphome                            (Pour utiliser les focntionnalités TCP)
+            bblanchon/ArduinoJson@^6.17.2               (Pour accéder au fonctionnalités Json)
+
+    Autres librairies (à copier dans le répertoire lib)
+         WifiManagerDevelopment
+            //Remarques
+            //Pour trouver le WifiManager (dans la branche development)
+            //   https://github.com/tzapu/WiFiManager/tree/development
+            //   Ne pas oublier d'appuyez sur l'ampoule et choisir : ajouter Lib
+    
+    Fonctions utiles (utilitaires)
+        /lib/MYLIB/myFunctions.cpp
+            //Pour vérifier plus simplement que deux chaines sont identiques
+            bool isEqualString(std::string line1, std::string line2)
+            //Pour extraire une partie d'une chaine de caractères avec l'aide d'un index
+            std::string getValue(std::string data, char separator, int index)
+            //Pour remplacer plus facilement une sous chaine
+            bool replaceAll(std::string& source, const std::string& from, const std::string& to)
+            //Pour obtenir un chaine aléatoire d'une certaine longeur
+            std::string get_random_string(unsigned int len)
+
+    Classes du système
+         
+        MyServer                        V1.0    Pour la gestion des routes pour le site WEB
+            /data                               Répertoire qui contient les fichiers du site WEB 
+                index.html              V1.0    Page index du site WEB
+                index.css               V1.0    CSS
+                script.js               V1.0    JS (fonctions JavaScript)
+              
+ * */
+#include <iostream>
+#include <string>
 #include <Arduino.h>
 #include <Wire.h>
 #include <String>
 #include "TemperatureStub.h"
- 
+#include "myFunctions.cpp"
+#include <HTTPClient.h>
+#include <WiFiManager.h>
 // Définition des LEDs
 #define GPIO_PIN_LED_LOCK_ROUGE 12 // Led Rouge GPIO12
 #define GPIO_PIN_LED_OK_VERT 14 // Led Verte GPIO14
